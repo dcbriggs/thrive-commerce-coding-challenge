4fdb5848383cbbdf438a9066cff81c9a8709327a
Don Briggs <don.c.briggs@gmail.com>
2023-03-08 14:18:38 -0800

    Add basic person, car models and seeds
    This commit add scaffolding with the person and car models along with
    seed data so you can see some informtion prepopulated when you go to
    their pages. If you run seeds and `rails s` you will see basic
    information on the `/cars` and `/people` pages. You can view, edit, add,
    and destroy the models from here.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+Person.create(name:'Henry Ford', email: 'henry@ford.com', telephone:'(555) 555-5555')
+Person.create(name:'Kiichiro Toyoda', email: 'kiichiro@toyota.com', telephone:'(123) 456-7890')
+Person.create(name:'Enzo Ferrari', email: 'enzo@ferrari.com', telephone:'(123) 123-4567')
+
+Car.create(make: 'Ford', model: 'Mustang', color: 'blue', mileage: 50000, is_for_sale: false)
+Car.create(make: 'Ford', model: 'Fusion', color: 'green', mileage: 75000, is_for_sale: false)
+Car.create(make: 'Toyota', model: 'Corolla', color: 'silver', mileage: 100000, is_for_sale: true)
+Car.create(make: 'Toyota', model: 'Camry', color: 'black', mileage: 25000, is_for_sale: false)
+Car.create(make: 'Ferrari', model: 'Roma', color: 'red', mileage: 5000, is_for_sale: false)
+Car.create(make: 'Ferrari', model: 'California', color: 'red', mileage: 10000, is_for_sale: false)
